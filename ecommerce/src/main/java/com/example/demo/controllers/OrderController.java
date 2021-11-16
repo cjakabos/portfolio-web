d9dd16b874b506cbba9891051bf7f4119a796d14
cjakabos <csaba.jakabos@gmail.com>
2021-11-16 07:56:50 +0100

    adding application files

---
+++ ecommerce/src/main/java/com/example/demo/controllers/OrderController.java
@@ -0,0 +1,52 @@
+package com.example.demo.controllers;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.model.persistence.*;
+import com.example.demo.model.persistence.repositories.*;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestController
+@RequestMapping("/api/order")
+public class OrderController {
+
+	public static final Logger log = LoggerFactory.getLogger(OrderController.class);
+
+	@Autowired
+	public UserRepository userRepository;
+	
+	@Autowired
+	public OrderRepository orderRepository;
+	
+	
+	@PostMapping("/submit/{username}")
+	public ResponseEntity<UserOrder> submit(@PathVariable String username) {
+		User user = userRepository.findByUsername(username);
+		if(user == null) {
+			log.error("User not found during order submit: " + user.getUsername());
+			return ResponseEntity.notFound().build();
+		}
+		UserOrder order = UserOrder.createFromCart(user.getCart());
+		orderRepository.save(order);
+		log.info("Userorder creation successful for : " + user.getUsername());
+		return ResponseEntity.ok(order);
+	}
+	
+	@GetMapping("/history/{username}")
+	public ResponseEntity<List<UserOrder>> getOrdersForUser(@PathVariable String username) {
+		User user = userRepository.findByUsername(username);
+		if(user == null) {
+			log.error("User not found during order history: " + user.getUsername());
+			return ResponseEntity.notFound().build();
+		}
+		log.info("User order history fetch is successful for : " + user.getUsername());
+		return ResponseEntity.ok(orderRepository.findByUser(user));
+	}
+}
---
+++ ecommerce/src/main/java/com/example/demo/controllers/UserController.java
@@ -0,0 +1,68 @@
+package com.example.demo.controllers;
+
+import java.util.Optional;
+
+import org.apache.logging.log4j.LogManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import com.example.demo.model.persistence.*;
+import com.example.demo.model.persistence.repositories.*;
+import com.example.demo.model.requests.*;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@RestController
+@RequestMapping("/api/user")
+public class UserController {
+
+	public static final Logger log = LoggerFactory.getLogger(UserController.class);
+
+	@Autowired
+	public UserRepository userRepository;
+	
+	@Autowired
+	public CartRepository cartRepository;
+
+	@Autowired
+	public BCryptPasswordEncoder  bCryptPasswordEncoder;
+	
+	@GetMapping("/id/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id) {
+		return ResponseEntity.of(userRepository.findById(id));
+	}
+	
+	@GetMapping("/{username}")
+	public ResponseEntity<User> findByUserName(@PathVariable String username) {
+		User user = userRepository.findByUsername(username);
+		return user == null ? ResponseEntity.notFound().build() : ResponseEntity.ok(user);
+	}
+	
+	@PostMapping("/create")
+	public ResponseEntity<User> createUser(@RequestBody CreateUserRequest createUserRequest) {
+		User user = new User();
+		
+		if (createUserRequest.getPassword().length() < 8) {
+			log.error("password is too short");
+			return ResponseEntity.badRequest().build();
+		}
+		if (!createUserRequest.getPassword().equals(createUserRequest.getConfirmPassword())) {
+			log.error("password do not match with confirm password");
+			return ResponseEntity.badRequest().build();
+		}
+		user.setUsername(createUserRequest.getUsername());
+		user.setPassword(this.bCryptPasswordEncoder.encode(createUserRequest.getPassword()));
+		Cart cart = new Cart();
+		cartRepository.save(cart);
+		user.setCart(cart);
+		userRepository.save(user);
+		log.info("user creation successful for: " + user.getUsername());
+		return ResponseEntity.ok(user);
+	}
+	
+}
