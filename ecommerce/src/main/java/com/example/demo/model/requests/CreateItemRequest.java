bbbe320de3d5f4f7aa858cc79b34c8f2f3b23c32
cjakabos <csaba.jakabos@gmail.com>
2021-11-20 15:38:34 +0100

    forgot to push new CreateItemRequest for generating new items

---
+++ ecommerce/src/main/java/com/example/demo/model/requests/CreateItemRequest.java
@@ -0,0 +1,55 @@
+package com.example.demo.model.requests;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+public class CreateItemRequest {
+    @JsonProperty
+    private Long id;
+
+    @JsonProperty
+    private String name;
+
+    @JsonProperty
+    private BigDecimal price;
+
+    @JsonProperty
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public BigDecimal getPrice() {
+        return price;
+    }
+
+    public void setPrice(BigDecimal price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
