3bd658b7108a638f77362074ab816e8e08103bee
cjakabos <csaba.jakabos@gmail.com>
2024-04-28 17:23:31 +0200

    modify cloudapp to RSA512 based authentication and add ngx-http-auth-jwt-module based auth to nginx reverse proxy

--- backend/cloudapp/src/main/java/com/example/demo/utilities/JwtUtilities.java
+++ backend/cloudapp/src/main/java/com/example/demo/utilities/JwtUtilities.java
@@ -12,33 +12,74 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Component;
 
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.NoSuchAlgorithmException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.InvalidKeySpecException;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.KeyFactory;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
 import java.util.Date;
-
-import static com.auth0.jwt.algorithms.Algorithm.HMAC512;
 
 @Component
 public class JwtUtilities {
-
-    @Value("testme")
-    private String apiSecret;
 
     private static final Logger logger = LoggerFactory.getLogger(JwtUtilities.class);
 
     @Autowired
     public UserRepository userRepository;
 
+    private static final String PUBLIC_KEY_FILE_RSA = "localhost+3-key.pub";
+    private static final String PRIVATE_KEY_FILE_RSA = "localhost+3-key.pem";
+
+    private static final String PEM_PUBLIC_START = "-----BEGIN PUBLIC KEY-----";
+    private static final String PEM_PUBLIC_END = "-----END PUBLIC KEY-----";
+
+    private static final String PEM_PRIVATE_START = "-----BEGIN PRIVATE KEY-----";
+    private static final String PEM_PRIVATE_END = "-----END PRIVATE KEY-----";
+
     public String generateToken(Authentication auth) {
+
+        RSAPrivateKey key;
+
+        try {
+            String privateKeyPem = new String(Files.readAllBytes(Paths.get(PRIVATE_KEY_FILE_RSA)));
+            privateKeyPem = privateKeyPem.replace(PEM_PRIVATE_START, "").replace(PEM_PRIVATE_END, "");
+            privateKeyPem = privateKeyPem.replaceAll("\\s", "");
+            byte[] pkcs8EncodedKey = Base64.getDecoder().decode(privateKeyPem);
+            PKCS8EncodedKeySpec spec = new PKCS8EncodedKeySpec(pkcs8EncodedKey);
+            key = (RSAPrivateKey) KeyFactory.getInstance("RSA").generatePrivate(spec);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException | IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+
         return JWT.create()
                 .withIssuer("cloudapp")
                 .withSubject(((org.springframework.security.core.userdetails.User) auth.getPrincipal()).getUsername())
                 .withExpiresAt(new Date(System.currentTimeMillis() + 864_000_000))
-                .sign(Algorithm.HMAC512(apiSecret));
+                .sign(Algorithm.RSA512(key));
     }
 
     public String getSubject(String token) {
+
+        RSAPublicKey key;
+        try {
+            String publicKeyPem = new String(Files.readAllBytes(Paths.get(PUBLIC_KEY_FILE_RSA)));
+            publicKeyPem = publicKeyPem.replace(PEM_PUBLIC_START, "").replace(PEM_PUBLIC_END, "");
+            publicKeyPem = publicKeyPem.replaceAll("\\s", "");
+            X509EncodedKeySpec data = new X509EncodedKeySpec(Base64.getDecoder().decode((publicKeyPem)));
+            key = (RSAPublicKey) KeyFactory.getInstance("RSA").generatePublic(data);
+        } catch (NoSuchAlgorithmException | InvalidKeySpecException | IOException e) {
+            throw new IllegalArgumentException(e);
+        }
+
         DecodedJWT verifier = null;
         try {
-            Algorithm algorithm = Algorithm.HMAC512(apiSecret);
+            Algorithm algorithm = Algorithm.RSA512(key);
             verifier = JWT.require(algorithm)
                     .withIssuer("cloudapp")
                     .build()
